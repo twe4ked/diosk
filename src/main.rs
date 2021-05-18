1c68fc8434b40cc933d20d5a79acaf37275999cd
Odin Dutton <odindutton@gmail.com>
2021-05-18 13:45:04 +1000

    Tweak ASCII logo

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,14 @@ use diosk::state::State;
 use diosk::terminal;
 use diosk::worker::Worker;
 
-//  ,gggggggggg,
-// dP"""88""""Y8b,                           ,dPYb,
-// Yb,  88     `8b,                          IP'`Yb
-//  `"  88      `8b gg                       I8  8I
-//      88       Y8 ""                       I8  8bgg,
-//      88       d8 gg    ,ggggg,    ,g,     I8 dP" "8
-//      88      ,8P 88   dP"  "Y8ggg,8'8,    I8d8bggP"
-//      88     ,8P' 88  i8'    ,8I ,8'  Yb   I8P' "Yb,
-//      88____,dP'_,88_,d8,   ,d8',8'_   8) ,d8    `Yb,
-//     8888888P"  8P""YP"Y8888P"  P' "YY8P8P88P      Y8
+//  ,ogggggggg,
+// dP"""88""""Y8b,                          ,dPYb,
+// Yb,  88     `8b,                         IP'`Yb
+//  `"  88     `8b'gg                       I8  8I
+//      88      d8'""    ,ggggg,    ,g,     I8 dP" "8
+//      88     ,8P 88   dP"  "Y8ggg,8'8,    I8d8bggP"
+//      88___,dP'_,88_,d8,   ,d8',8'_   8) ,d8    `Yb,
+//     888888P"  8P""YP"Y8888P"  P' "YY8P8P88P      Y8
 
 fn main() {
     simple_logging::log_to_file("target/out.log", log::LevelFilter::Info)
