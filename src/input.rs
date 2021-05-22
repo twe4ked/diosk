0131682e698638a198e83b8df1bacd3e8de5c833
Odin Dutton <odindutton@gmail.com>
2021-05-22 16:45:38 +1000

    Extract handle_key_event function

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 
-use crossterm::event::{read, Event, KeyCode, KeyModifiers};
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 use log::info;
 
 use crate::state::{Mode, State};
@@ -15,39 +15,7 @@ pub fn run(state: Arc<Mutex<State>>) {
         let mut state = state.lock().expect("poisoned");
 
         match event {
-            Event::Key(event) => {
-                let mode = state.mode.clone();
-
-                state.clear_error_message();
-
-                match mode {
-                    Mode::Normal | Mode::Loading => match event.code {
-                        KeyCode::Char(':') => state.input(),
-                        KeyCode::Char('j') => state.down(),
-                        KeyCode::Char('k') => state.up(),
-                        KeyCode::Enter => {
-                            state.enter();
-                        }
-                        _ => {}
-                    },
-
-                    Mode::Input => match (event.code, event.modifiers) {
-                        (KeyCode::Char(c), KeyModifiers::NONE) => state.input_char(c),
-                        (KeyCode::Enter, _) => state.enter(),
-                        (KeyCode::Esc, _) => state.cancel_input_mode(),
-                        (_, _) => {
-                            if let Some(command) = edit::command(event) {
-                                match command {
-                                    Command::DeleteWord => state.delete_word(),
-                                    Command::DeleteChar => state.delete_char(),
-                                }
-                            }
-                        }
-                    },
-                }
-
-                info!("{:?}", &state);
-            }
+            Event::Key(event) => handle_key_event(&mut state, event),
             Event::Mouse(event) => info!("{:?}", event),
             Event::Resize(width, height) => state.new_size(width, height),
         }
@@ -56,4 +24,38 @@ pub fn run(state: Arc<Mutex<State>>) {
             break;
         }
     }
+}
+
+fn handle_key_event(state: &mut State, event: KeyEvent) {
+    let mode = state.mode.clone();
+
+    state.clear_error_message();
+
+    match mode {
+        Mode::Normal | Mode::Loading => match event.code {
+            KeyCode::Char(':') => state.input(),
+            KeyCode::Char('j') => state.down(),
+            KeyCode::Char('k') => state.up(),
+            KeyCode::Enter => {
+                state.enter();
+            }
+            _ => {}
+        },
+
+        Mode::Input => match (event.code, event.modifiers) {
+            (KeyCode::Char(c), KeyModifiers::NONE) => state.input_char(c),
+            (KeyCode::Enter, _) => state.enter(),
+            (KeyCode::Esc, _) => state.cancel_input_mode(),
+            (_, _) => {
+                if let Some(command) = edit::command(event) {
+                    match command {
+                        Command::DeleteWord => state.delete_word(),
+                        Command::DeleteChar => state.delete_char(),
+                    }
+                }
+            }
+        },
+    }
+
+    info!("{:?}", &state);
 }
